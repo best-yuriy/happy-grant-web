f1db3f4f36be1c6d745cf3d768bb84a63e96319b
best-yuriy <119270583+best-yuriy@users.noreply.github.com>
2024-12-13 20:54:23 -0600

    chore: add firestore + polyfills for tests

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,9 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import '@inrupt/jest-jsdom-polyfills';
+import { ReadableStream } from "web-streams-polyfill";
+import { setImmediate } from 'timers';
+
+global.ReadableStream = new ReadableStream();
+global.setImmediate = setImmediate;
